c0a103086181e5ade8b72e060ba5a76894312981
Techy-Suryansh0001 <suryanshsingh1208@gmail.com>
2026-02-16 10:26:57 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Welcome to Btech G2G");
+    printf("Enter the Value of x and y");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("sum is %d",z);
+    return 0;
+}
